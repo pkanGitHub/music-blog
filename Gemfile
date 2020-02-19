8bd5c86f0df26aa28df10c842bf9ca8fd9bf8337
Phyllis Kan <Phyllis Kan>
2020-02-19 12:57:27 -0600

    Set up omniauth

--- Gemfile
+++ Gemfile
@@ -56,3 +56,9 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "devise", "~> 4.7"
 
 gem "pry", "~> 0.12.2"
+
+gem "omniauth-facebook", "~> 6.0"
+
+gem "dotenv-rails", "~> 2.7"
+
+gem "omniauth", "~> 1.9"
---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,18 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+    def facebook
+        # byebug
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+
+        if @user.persisted?
+            sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+        else
+            session["devise.facebook_data"] = request.env["omniauth.auth"]
+            redirect_to new_user_registration_url
+        end
+    end
+
+    def failure
+        # byebug
+    end
+end
